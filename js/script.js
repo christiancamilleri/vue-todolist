a8f354e5e01d6491c431456e3a4126c00c13c62d
christiancamilleri <camilleri.christian@outlook.com>
2023-03-15 13:52:21 +0100

    feat: milestone 2

--- js/script.js
+++ js/script.js
@@ -15,8 +15,22 @@ createApp({
         {
             text: "aperitivo",
             done: true,
+        }, 
+        {
+            text: "portare fuori il cane",
+            done: true,
+        },
+        {
+            text: "vedermi con tizio",
+            done: false,
         }
-      ]
+      ],
     }
+  },
+  methods: {
+     removeText(index) {
+        this.toDo.splice(index, 1)
+     }
   }
+
 }).mount('#app')
